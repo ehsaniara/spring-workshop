c0a587459677da95e292620a655f382fefffc3c3
Jay Ehsaniara <ehsaniara@gmail.com>
2022-09-21 12:17:56 -0700

    Add main method in Dealership.java

---
+++ dealership/src/main/java/com/springworkshop/dealership/Dealership.java
@@ -0,0 +1,12 @@
+package com.springworkshop.dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dealership {
+    public static void main(String[] args) {
+        SpringApplication.run(Dealership.class, args);
+    }
+
+}
