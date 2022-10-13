8b6783c9b4f7813933018fff3d992f7502ab13a2
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-10-13 12:32:56 -0700

    Add integration tests

---
+++ dealership/src/test/java/com/springworkshop/dealership/IntegrationTestBaseClass.java
@@ -0,0 +1,27 @@
+package com.springworkshop.dealership;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+//@EnableJpaRepositories
+//@EntityScan("com.springworkshop.dealership.domain")
+public class IntegrationTestBaseClass {
+    @Container
+    protected static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14-alpine");
+    @LocalServerPort
+    protected int port;
+
+    @DynamicPropertySource
+    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
